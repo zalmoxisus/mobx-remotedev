3de84b1f1081e807ff7bd441a4b101e6ef9dcf7e
Zalmoxisus <zalmoxisus@gmail.com>
2016-07-20 14:39:54 +0300

    Add simple-todo example

---
+++ examples/simple-todo/index.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import mobx from 'mobx';
+import mobxReact from 'mobx-react';
+import remotedev from '../../src/dev';
+
+mobx.useStrict(true);
+var todoFactory = function (title) {
+
+  var todo = {
+    id: Math.random(),
+    toggleStatus: mobx.action(function toggleStatus() {
+      todo.finished = !todo.finished;
+    })
+  };
+  mobx.extendObservable(todo,
+    {
+      title: title,
+      finished: false
+    }
+  );
+  return todo;
+};
+
+var todoListFactory = function () {
+  var todoList = {
+    addTodo: mobx.action(function addTodo (todo) {
+      todoList.todos.push(todo);
+    }),
+    addTodos: mobx.action(function addTodos (todos) {
+      todoList.todos = todoList.todos.concat(todos);
+    })
+  };
+
+  mobx.extendObservable(todoList, {
+    todos: [],
+    unfinishedTodoCount: function () {
+      return todoList.todos.filter(function (todo) {
+        return !todo.finished;
+      }).length;
+    }
+  });
+
+  return remotedev(todoList);
+};
+
+var TodoListView = mobxReact.observer(function TodoListView() {
+    var listItems = this.props.todoList.todos.map(function (todo) {
+      return React.createElement(TodoView, {todo: todo, key: todo.id});
+    });
+
+    return React.createElement('div', null,
+      React.createElement('ul', null, listItems),
+      'Tasks left: ' + this.props.todoList.unfinishedTodoCount
+    );
+  }
+);
+
+var TodoView = mobxReact.observer(
+  React.createClass({
+    displayName: 'TodoView',
+    render: function () {
+      var todo = this.props.todo;
+      return React.createElement('li', null,
+        React.createElement('input', {
+          type: 'checkbox',
+          checked: todo.finished,
+          onClick: this.selectHandler
+        }),
+        todo.title
+      );
+    },
+    selectHandler: function () {
+      this.props.todo.toggleStatus();
+    }
+  })
+);
+
+var store = todoListFactory();
+
+ReactDOM.render(
+  React.createElement(TodoListView, {todoList: store}),
+  document.getElementById('root')
+);
+
+store.addTodos([todoFactory('Get Coffee'), todoFactory('Write simpler code')]);
+store.todos[0].toggleStatus(true);
