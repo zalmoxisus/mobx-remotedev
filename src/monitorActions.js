2f89755bd4a6a4cfc5eb9c3e044094c6fc998df9
Zalmoxisus <zalmoxisus@gmail.com>
2016-07-24 19:06:08 +0300

    Implement COMMIT and ROLLBACK actions

--- src/monitorActions.js
+++ src/monitorActions.js
@@ -5,6 +5,7 @@ import { setValue } from './utils';
 export const isMonitorAction = (store) => store.__isRemotedevAction === true;
 
 export const dispatchMonitorAction = (store, devTools) => {
+  let intermValue;
   const initValue = mobx.toJS(store);
   devTools.init(initValue);
 
@@ -14,6 +15,14 @@ export const dispatchMonitorAction = (store, devTools) => {
         case 'RESET':
           setValue(store, initValue);
           devTools.init(initValue);
+          return;
+        case 'COMMIT':
+          intermValue = mobx.toJS(store);
+          devTools.init(intermValue);
+          return;
+        case 'ROLLBACK':
+          setValue(store, intermValue);
+          devTools.init(intermValue);
           return;
         case 'JUMP_TO_STATE':
           setValue(store, parse(message.state));
