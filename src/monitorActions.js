c733ad4f53f3224add01c847fedbf975d6805e94
Zalmoxisus <zalmoxisus@gmail.com>
2016-07-27 21:42:56 +0300

    Toggle (skip) action

--- src/monitorActions.js
+++ src/monitorActions.js
@@ -1,6 +1,6 @@
 import mobx from 'mobx';
 import { stringify, parse } from 'jsan';
-import { setValue } from './utils';
+import { silently, setValue } from './utils';
 
 export const isMonitorAction = (store) => store.__isRemotedevAction === true;
 
@@ -10,6 +10,32 @@ function dispatchRemotely(store, { type, arguments: args }) {
     return;
   }
   store[type](...args);
+}
+
+function toggleAction(store, id, strState) {
+  const liftedState = parse(strState);
+  const idx = liftedState.skippedActionIds.indexOf(id);
+  const skipped = idx !== -1;
+  const start = liftedState.stagedActionIds.indexOf(id);
+  if (start === -1) return liftedState;
+
+  setValue(store, liftedState.computedStates[start - 1].state);
+  for (let i = (skipped ? start : start + 1); i < liftedState.stagedActionIds.length; i++) {
+    if (
+      i !== start && liftedState.skippedActionIds.indexOf(liftedState.stagedActionIds[i]) !== -1
+    ) continue; // it's already skipped
+    silently(() => {
+      dispatchRemotely(store, liftedState.actionsById[liftedState.stagedActionIds[i]].action);
+    }, store);
+    liftedState.computedStates[i].state = mobx.toJS(store);
+  }
+
+  if (skipped) {
+    liftedState.skippedActionIds.splice(idx, 1);
+  } else {
+    liftedState.skippedActionIds.push(id);
+  }
+  return liftedState;
 }
 
 export const dispatchMonitorAction = (store, devTools) => {
@@ -30,6 +56,9 @@ export const dispatchMonitorAction = (store, devTools) => {
           return;
         case 'JUMP_TO_STATE':
           setValue(store, parse(message.state));
+          return;
+        case 'TOGGLE_ACTION':
+          devTools.send(null, toggleAction(store, message.payload.id, message.state));
           return;
       }
     } else if (message.type === 'ACTION') {
