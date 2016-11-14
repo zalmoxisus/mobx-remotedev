5933dbf4118007523e137b29f46b5868c4b3d852
Zalmoxisus <zalmoxisus@gmail.com>
2016-11-14 19:06:11 +0200

    Apply filters only if window is defined (fix node support)

--- src/filters.js
+++ src/filters.js
@@ -1,4 +1,5 @@
 export function isFiltered(action, localFilter) {
+  if (typeof window === 'undefined' && !localFilter) return true;
   if (
     !localFilter && !window.devToolsOptions ||
     !window.devToolsOptions.filter || window.devToolsOptions.filter === 'DO_NOT_FILTER'
