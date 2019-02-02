dfce5697610bcaa26c99ab4574f3ea663538fb8f
Ernest Francis <devin_98@msn.com>
2019-02-02 17:46:54 -0500

    fixed index.d.ts file to label config object/parameters as optional

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,17 @@
 /**
  * Wrapper/Decorator for Mobx stores
  */
-export default function<T>(store: T, config: RemoteDevConfig): T;
+export default function<T>(store: T, config?: RemoteDevConfig): T;
 
 /**
  * Config object for remotedev function argument
  */
 export type RemoteDevConfig = {
-  name: string;
-  onlyActions: Boolean;
-  global: Boolean;
-  filters: object;
-  remote: boolean;
-  hostname: string;
-  port: number;
+  name?: string;
+  onlyActions?: Boolean;
+  global?: Boolean;
+  filters?: object;
+  remote?: boolean;
+  hostname?: string;
+  port?: number;
 };
