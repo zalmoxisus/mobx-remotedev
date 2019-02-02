421ea8136b85aedb41cd326a5661c3ec9d0f0f7e
Ernest Francis <devin_98@msn.com>
2019-02-02 17:42:09 -0500

    fixed index.d.ts to include config object as second argument.

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,17 @@
-export default function<T>(store: T): T;
+/**
+ * Observer
+ */
+export default function<T>(store: T, config: RemoteDevConfig): T;
+
+/**
+ * Config object for remotedev function argument
+ */
+export type RemoteDevConfig = {
+  name: string;
+  onlyActions: Boolean;
+  global: Boolean;
+  filters: object;
+  remote: boolean;
+  hostname: string;
+  port: number;
+};
